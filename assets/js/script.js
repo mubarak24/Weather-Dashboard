ac4e5e4e50478be529a2c8a905d27d76db0d4900
mubarak <mubarakcali24@gmail.com>
2023-04-02 13:08:20 -0700

    cities in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,12 @@ var currentWeatherContainer = document.getElementById("current-weather");
 var forecastContainer = document.getElementById("forecast");
 var searchHistory = document.getElementById("search-history");
 
-clickBtn.addEventListener("click", function() {
+var searchHistoryData = [];
+if (localStorage.getItem("searchHistoryData")) {
+  searchHistoryData = JSON.parse(localStorage.getItem("searchHistoryData"));
+}
+
+clickBtn.addEventListener("click", function () {
   var city = searchInput.value.trim();
 
   // CURRENT WEATHER
@@ -16,7 +21,7 @@ clickBtn.addEventListener("click", function() {
       console.log(data);
 
       var cityName = data.name;
-      var currentDate = moment().format("M/D/YYYY");
+      var currentDate = new Date();
       var iconCode = data.weather[0].icon;
       var iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
       var temperature = data.main.temp;
@@ -29,31 +34,41 @@ clickBtn.addEventListener("click", function() {
       var cityEl = document.createElement("h2");
       cityEl.textContent = `${cityName} (${currentDate})`;
       currentWeatherEl.appendChild(cityEl);
-
+      
       var iconEl = document.createElement("img");
       iconEl.setAttribute("src", iconUrl);
       iconEl.setAttribute("alt", data.weather[0].description);
       currentWeatherEl.appendChild(iconEl);
-
+      
       var tempEl = document.createElement("p");
       tempEl.innerHTML = `Temperature: ${temperature}&deg;F`;
       currentWeatherEl.appendChild(tempEl);
-
-      var humidityEl = document.createElement("p");
-      humidityEl.textContent = `Humidity: ${humidity}%`;
-      currentWeatherEl.appendChild(humidityEl);
-
+      
       var windEl = document.createElement("p");
       windEl.textContent = `Wind Speed: ${windSpeed} MPH`;
       currentWeatherEl.appendChild(windEl);
-
+      
+      var humidityEl = document.createElement("p");
+      humidityEl.textContent = `Humidity: ${humidity}%`;
+      currentWeatherEl.appendChild(humidityEl);
+      
       // Append current weather to container
       currentWeatherContainer.innerHTML = "";
       currentWeatherContainer.appendChild(currentWeatherEl);
     })
     
-  // FORECAST
-  fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInput.value + '&units=imperial&appid=26f261122333e7edeba0c7e85805e21e')
+    searchHistoryData.push(city);
+    localStorage.setItem("searchHistoryData", JSON.stringify(searchHistoryData));
+    searchHistory.innerHTML = "";
+  
+    searchHistoryData.forEach(function (city) {
+      var li = document.createElement("li");
+      li.textContent = city;
+      searchHistory.appendChild(li);
+    });
+    
+    // FORECAST
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInput.value + '&units=imperial&appid=26f261122333e7edeba0c7e85805e21e')
     .then(function (response) {
       return response.json();
     })
