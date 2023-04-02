e7a80c38dde9fcbbd11ee78c4efca367f63a34f0
mubarak <mubarakcali24@gmail.com>
2023-04-02 11:23:35 -0700

    api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,63 @@
-
 var clickBtn = document.getElementById("click");
 var searchInput = document.getElementById("search");
+var currentWeatherContainer = document.getElementById("current-weather");
+var forecastContainer = document.getElementById("forecast");
+var searchHistory = document.getElementById("search-history");
 
 clickBtn.addEventListener("click", function() {
-    // grab the value from the input and console log it
-    console.log(searchInput.value);
-    // CURRENT WEATHER
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ searchInput.value  +'&units=imperial&appid=26f261122333e7edeba0c7e85805e21e')
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
+  var city = searchInput.value.trim();
 
-    //   FORECAST
-      fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ searchInput.value  +'&units=imperial&appid=26f261122333e7edeba0c7e85805e21e')
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
+  // CURRENT WEATHER
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchInput.value + '&units=imperial&appid=26f261122333e7edeba0c7e85805e21e')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      var cityName = data.name;
+      var currentDate = moment().format("M/D/YYYY");
+      var iconCode = data.weather[0].icon;
+      var iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+      var temperature = data.main.temp;
+      var humidity = data.main.humidity;
+      var windSpeed = data.wind.speed;
+
+      var currentWeatherEl = document.createElement("div");
+      currentWeatherEl.classList.add("current-weather");
+
+      var cityEl = document.createElement("h2");
+      cityEl.textContent = `${cityName} (${currentDate})`;
+      currentWeatherEl.appendChild(cityEl);
+
+      var iconEl = document.createElement("img");
+      iconEl.setAttribute("src", iconUrl);
+      iconEl.setAttribute("alt", data.weather[0].description);
+      currentWeatherEl.appendChild(iconEl);
+
+      var tempEl = document.createElement("p");
+      tempEl.innerHTML = `Temperature: ${temperature}&deg;F`;
+      currentWeatherEl.appendChild(tempEl);
+
+      var humidityEl = document.createElement("p");
+      humidityEl.textContent = `Humidity: ${humidity}%`;
+      currentWeatherEl.appendChild(humidityEl);
+
+      var windEl = document.createElement("p");
+      windEl.textContent = `Wind Speed: ${windSpeed} MPH`;
+      currentWeatherEl.appendChild(windEl);
+
+      // Append current weather to container
+      currentWeatherContainer.innerHTML = "";
+      currentWeatherContainer.appendChild(currentWeatherEl);
+    })
     
-// // create the element
-// var h1 = document.createElement("h1")
-// // add the text content
-// h1.textContent = "cities"
-// // append to the container
-// testContainer.append(h1)
+  // FORECAST
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInput.value + '&units=imperial&appid=26f261122333e7edeba0c7e85805e21e')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 });
